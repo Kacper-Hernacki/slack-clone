99b000001c51183401fd1db4ca1882e8c647a5c4
Kacper <hernackikacper@gmail.com>
2020-09-26 06:47:55 +0200

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6buDz-IkE2W8DmXozauRaVTkH_t3jidw",
+  authDomain: "slack-clone-828e9.firebaseapp.com",
+  databaseURL: "https://slack-clone-828e9.firebaseio.com",
+  projectId: "slack-clone-828e9",
+  storageBucket: "slack-clone-828e9.appspot.com",
+  messagingSenderId: "117121166515",
+  appId: "1:117121166515:web:a9c8b4054401e006b1d4c6",
+  measurementId: "G-LZPM49HB9S",
+};
